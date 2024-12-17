11c883b6d1f37a496abf1e23527ce246dd229125
James Kelly <jameskelly21699@gmail.com>
2024-12-17 22:39:46 +0000

    Revert "Replace file extensions"
    This reverts commit 0b34f971eb244177e623fe3086ffeb4b53ff30a4.

--- src/boost/archive/impl/yaml_grammar.cpp
+++ src/boost/archive/impl/yaml_grammar.cpp
@@ -59,7 +59,7 @@ template <> void yaml_grammar::init_chset()
 } // namespace archive
 } // namespace boost
 
-#include "basic_yaml_grammar.inl"
+#include "basic_yaml_grammar.hpp"
 
 namespace boost {
 namespace archive {
